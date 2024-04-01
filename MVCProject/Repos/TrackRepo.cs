77e8461f17625da74943f9b889eb864ea2723476
abdelrahman <abdelrahman.kobia1@gmail.com>
2024-04-01 15:06:19 +0200

    Adding Repos

---
+++ MVCProject/Repos/TrackRepo.cs
@@ -0,0 +1,74 @@
+﻿using MVCProject.Data;
+using MVCProject.Models;
+using System.Diagnostics;
+
+namespace MVCProject.Repos
+{
+    public interface ITrackRepo
+    {
+        public void AddTrack(Track t);
+        public void RemoveTrack(int tid);
+        public Track DeactivateTrack(int tid);
+        public void UpdateTrack(Track t);
+        public Track GetTrackById(int tid);
+
+        public Track GetTrackBySupervisor(int sid);
+
+
+        
+
+    }
+    public class TrackRepo : ITrackRepo
+    {
+        private attendanceDBContext db;
+        public void RemoveTrack(int trackId)
+        {
+            var trackToRemove = db.Tracks.Find(trackId);
+            if (trackToRemove != null)
+            {
+                db.Tracks.Remove(trackToRemove);
+                db.SaveChanges();
+            }
+        }
+
+        public TrackRepo(attendanceDBContext _db)
+        {
+            db = _db;
+        }
+
+        public void AddTrack(Track track)
+        {
+            db.Tracks.Add(track);
+            db.SaveChanges();
+        }
+
+        public Track DeactivateTrack(int trackId)
+        {
+            var track = db.Tracks.FirstOrDefault(t=>t.Id==trackId);
+            if (track != null)
+            {
+                track.Status = "Inactive";
+                db.SaveChanges();
+            }
+            return track;
+        }
+
+        public Track GetTrackById(int trackId)
+        {
+            return db.Tracks.FirstOrDefault(t=>t.Id ==trackId);
+        }
+
+
+        public Track GetTrackBySupervisor(int supervisorId)
+        {
+            return db.Tracks.FirstOrDefault(t => t.SupervisorID == supervisorId);
+        }
+
+
+        public void UpdateTrack(Track track)
+        {
+            db.Tracks.Update(track);
+            db.SaveChanges();
+        }
+    }
+}
