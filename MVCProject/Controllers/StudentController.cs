0416b62904732d308defa2095739967f796c4ecb
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-04-03 14:36:35 +0200

    Add Student Dashboard and Permission Management

---
+++ MVCProject/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCProject.Data;
+using MVCProject.Models;
+using MVCProject.Repos;
+
+namespace MVCProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepo stdRepo;
+
+        public StudentController(IStudentRepo _stdRepo)
+        {
+            stdRepo = _stdRepo;
+        }
+
+        public IActionResult Index(int id)
+        {
+            ViewBag.StudentId = id;
+            Student std = stdRepo.GetStudentByID(id);
+            return View(std);
+        }
+
+        public IActionResult ShowPermissions(int id)
+        {
+            ViewBag.Student = stdRepo.GetStudentByID(id);
+            ViewBag.StudentId = id;
+            var permissions = stdRepo.GetStudentPermissions(id);
+            return View(permissions);
+        }
+        [HttpGet]
+        public IActionResult RequestPermission(int id)
+        {
+            ViewBag.StudentId = id;
+            ViewBag.Student = stdRepo.GetStudentByID(id);
+            return View(new Permission());
+        }
+
+        [HttpPost]
+        public IActionResult RequestPermission(Permission permission)
+        {
+            stdRepo.RequestPermission(permission);
+            ViewBag.Message = "Permission Requested Successfully";
+            return RedirectToAction("ShowPermissions", new { id = permission.StdID });
+        }
+
+        public IActionResult DeletePermission(int permissionId,int stdId) 
+        {
+            stdRepo.DeletePermission(permissionId);
+            ViewBag.Message = "Permission Deleted Successfully";
+            return RedirectToAction("ShowPermissions", new { id = stdId });
+        }
+       
+
+      
+
+        
+    }
+}
--- MVCProject/Repos/StudentRepo.cs
+++ MVCProject/Repos/StudentRepo.cs
@@ -7,6 +7,16 @@ namespace MVCProject.Repos
     {
         public Student GetStudentByEmailAndPassword(string email, string password);
         public void RegisterStudent(Student std);
+
+        public Student GetStudentByID(int id);
+
+        public IEnumerable<Permission> GetStudentPermissions(int id);
+
+        public void RequestPermission(Permission permission);
+
+        public void DeletePermission(int permissionId);
+
+        public bool StudentExists(int id);
     }
 
     public class StudentRepo : IStudentRepo
@@ -26,8 +36,55 @@ namespace MVCProject.Repos
             db.SaveChanges();
             if (std.Id == 0)
             {
-                throw new System.Exception("Error in saving student");
+                throw new Exception("Error in saving student");
             }
         }
+
+        public Student GetStudentByID(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public IEnumerable<Permission> GetStudentPermissions(int id)
+        {
+            return db.Permissions.Where(p => p.StdID == id).OrderByDescending(p => p.Id).ToList();
+        }
+
+
+        public void RequestPermission(Permission permission)
+        {
+
+            try
+            {
+                db.Permissions.Add(permission);
+                db.SaveChanges();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+
+
+        public void DeletePermission(int permissionId)
+        {
+            try
+            {
+                Permission permission = db.Permissions.Find(permissionId);
+                db.Permissions.Remove(permission);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public bool StudentExists(int id) { return db.Students.Any(e => e.Id == id); }
     }
 }
