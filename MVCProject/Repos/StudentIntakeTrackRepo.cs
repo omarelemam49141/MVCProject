ec6882b69d0ebddaee0030c1ec879a34c71da583
omarelemam49141 <114430558+omarelemam49141@users.noreply.github.com>
2024-04-16 18:27:27 +0200

    finishing employee show records

--- MVCProject/Repos/StudentIntakeTrackRepo.cs
+++ MVCProject/Repos/StudentIntakeTrackRepo.cs
@@ -1,14 +1,29 @@
-﻿using MVCProject.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject.Data;
+using MVCProject.Models;
 
 namespace MVCProject.Repos
 {
-    public class StudentIntakeTrackRepo
+    public interface IStudentIntakeTrackRepo
+    {
+        List<StudentIntakeTrack> GetByTrackAndIntakeIDs(int trackID, int intakeID);
+    }
+    public class StudentIntakeTrackRepo: IStudentIntakeTrackRepo
     {
         private attendanceDBContext db;
 
         public StudentIntakeTrackRepo(attendanceDBContext _db) 
         {
             db = _db;
+        }
+
+        public List<StudentIntakeTrack> GetByTrackAndIntakeIDs(int trackID, int intakeID)
+        {
+            return db
+                    .StudentIntakeTracks
+                    .Include(sit=>sit.Student)
+                    .Where(sit=>sit.IntakeID==intakeID && sit.TrackID==trackID)
+                    .ToList();
         }
     }
 }
